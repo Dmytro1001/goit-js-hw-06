cd598a9777c34f104f64ca6d67154e188d48eb4b
Dmytro1001 <31dmitri13@gmail.com>
2022-12-04 12:37:21 +0200

    task-4 = +-done;

---
+++ js/task-04.js
@@ -0,0 +1,52 @@
+
+const CounterPlugin = function ({ rootSelector, initialValue = 0, step = 1 } = {}) {
+    this.rootSelector = rootSelector;
+    this.value = initialValue;
+    this.step = step;
+    this.refs = this.getRefs(rootSelector);
+
+    this.bindEvents();
+    this.updateValue();
+};
+
+CounterPlugin.prototype.getRefs = function (rootSelector) {
+    const refs = {};
+    refs.container = document.querySelector(rootSelector);
+    refs.incrementBtn = refs.container.querySelector('[data-action="increment"]');
+    refs.decrementBtn = refs.container.querySelector('[data-action="decrement"]');
+    refs.value = refs.container.querySelector('#value');
+
+    return refs;
+}
+
+CounterPlugin.prototype.bindEvents = function () {
+    this.refs.incrementBtn.addEventListener('click', () => {
+        this.increment();
+        this.updateValue();
+    });
+
+    this.refs.decrementBtn.addEventListener('click', () => {
+        this.decrement();
+        this.updateValue();
+    });
+};
+
+CounterPlugin.prototype.updateValue = function () {
+    this.refs.value.textContent = this.value;
+}
+
+CounterPlugin.prototype.increment = function () {
+    this.value += this.step;
+};
+
+CounterPlugin.prototype.decrement = function () {
+    this.value -= this.step;
+}
+
+const counterValue = new CounterPlugin({rootSelector: '#counter', step: 1 });
+
+
+
+// Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+// Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+// Оновлюй інтерфейс новим значенням змінної counterValue.
