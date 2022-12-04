4bd811dbddd56d852fcbaac8e58651bda415a96d
Dmytro1001 <31dmitri13@gmail.com>
2022-12-05 00:09:05 +0200

    Task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const body = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const res = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();;
+  res.innerText = getRandomHexColor();;
+
+  function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+  .toString(16)
+      .padStart(6, 0)}`;
+};
+})
